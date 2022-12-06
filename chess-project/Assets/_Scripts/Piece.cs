6205829fe45fd7fbe807f28b2e2cbbe8a2ab24d1
Vik <v.benini99@gmail.com>
2022-12-06 15:13:01 +0100

    Converted Pieces to gameObject
    Pieces are know gameObjects.
     - Einfacher Komponenten hinzufügen
     - Unterstützt mehr komponenten

--- chess-project/Assets/_Scripts/Piece.cs
+++ chess-project/Assets/_Scripts/Piece.cs
@@ -13,38 +13,44 @@ public class Piece : MonoBehaviour
 
     [SerializeField] SpriteRenderer _renderer;
 
+    // Variable to store movement of the piece
+    // ...
+    // Variable to ...
+   
+
+
+    /**
+    Loads the required sprite and adds it to the renderer.
+    **/
+    public Sprite GetSprite(string pieceName){
+        Sprite [] _spArray = Resources.LoadAll<Sprite>("Pieces");
+
+        // piece name like the sprite name -> 1 after the name indicates black
+        switch(pieceName){
+            case("bishop"):  return _spArray[0];
+            case("bishop1"): return _spArray[1];
+            case("king"):    return _spArray[2];
+            case("king1"):   return _spArray[3];
+            case("knight"):  return _spArray[4];
+            case("knight1"): return _spArray[5];
+            case("pawn"):    return _spArray[6];
+            case("pawn1"):   return _spArray[7];
+            case("queen"):   return _spArray[8];
+            case("queen1"):  return _spArray[9];
+            case("rook"):    return _spArray[10];
+            case("rook1"):   return _spArray[11];
+
+        }
+        return null;
+        
+    }
+
     /** 
     Sets the color of the renderer. Can be used to color the pieces 
     in a different color.
     **/
     public void SetColor(Color color){
         _renderer.color = color;    
-    }
-
-
-    /**
-    Loads the required sprite and adds it to the renderer.
-    **/
-    public void SetSprite(string pieceName){
-        Sprite [] _spArray = Resources.LoadAll<Sprite>("Pieces");
-
-        // piece name like the sprite name -> 1 after the name indicates black
-        switch(pieceName){
-            case("bishop"): _renderer.sprite = _spArray[0]; break;
-            case("bishop1"):_renderer.sprite = _spArray[1]; break;
-            case("king"):   _renderer.sprite = _spArray[2]; break;
-            case("king1"):  _renderer.sprite = _spArray[3]; break;
-            case("knight"): _renderer.sprite = _spArray[4]; break;
-            case("knight1"):_renderer.sprite = _spArray[5]; break;
-            case("pawn"):   _renderer.sprite = _spArray[6]; break;
-            case("pawn1"):  _renderer.sprite = _spArray[7]; break;
-            case("queen"):  _renderer.sprite = _spArray[8]; break;
-            case("queen1"): _renderer.sprite = _spArray[9]; break;
-            case("rook"):   _renderer.sprite = _spArray[10]; break;
-            case("rook1"):  _renderer.sprite = _spArray[11]; break;
-
-        }
-        
     }
 
 }
--- chess-project/Assets/_Scripts/PieceManager.cs
+++ chess-project/Assets/_Scripts/PieceManager.cs
@@ -10,45 +10,80 @@ public class PieceManager : MonoBehaviour
 {
 
     [SerializeField] Piece _piecePrefab;
+
+    public Piece _piece;
     
     /**
     Generates the Pieces on the board by the providing the x and y values of the tile positions.
+
+    TODO: Can be done prettier!
     **/
     public void PlacePieces(int x, int y){
 
         // Create a row of pawns at the for both players...
         if(y == 1 || y == 6){
             Debug.Log(System.String.Format("Tile found: {0},{1}", x, y));
-            var spawnedPiece = Instantiate(_piecePrefab, new Vector3(x, y), Quaternion.identity);
-            spawnedPiece.name = $"Pawn{y}";
-            spawnedPiece.SetSprite(y == 1 ? "pawn" : "pawn1");
+
+            GameObject spawnedPiece = new GameObject($"Pawn {x},{y}");
+            spawnedPiece.transform.position = new Vector3(x, y, -3);
+            
+            _piece = spawnedPiece.AddComponent<Piece>();
+            
+            SpriteRenderer _renderer = spawnedPiece.AddComponent<SpriteRenderer>();
+            _renderer.sprite = _piece.GetSprite(y == 1 ? "pawn" : "pawn1");
         }
+
+
+
+
+
+
 
         if(y == 0 || y == 7){
             if(x == 0 || x == 7){
-                    var spawnedPiece = Instantiate(_piecePrefab, new Vector3(x, y), Quaternion.identity);
-                    spawnedPiece.name = $"Rook{y}";
-                    spawnedPiece.SetSprite(y == 0 ? "rook" : "rook1");
+                    GameObject spawnedPiece = new GameObject($"Rook {x},{y}");
+                    spawnedPiece.transform.position = new Vector3(x, y, -3);
+            
+                    _piece = spawnedPiece.AddComponent<Piece>();
+            
+                    SpriteRenderer _renderer = spawnedPiece.AddComponent<SpriteRenderer>();
+                    _renderer.sprite = _piece.GetSprite(y == 0 ? "rook" : "rook1");
             }
-            else if(x == 1 || x == 6){
-                var spawnedPiece = Instantiate(_piecePrefab, new Vector3(x, y), Quaternion.identity);
-                    spawnedPiece.name = $"Knight{y}";
-                    spawnedPiece.SetSprite(y == 0 ? "knight" : "knight1");
+            else if(x == 1 || x == 6){  
+                    GameObject spawnedPiece = new GameObject($"Knight {x},{y}");
+                    spawnedPiece.transform.position = new Vector3(x, y, -3);
+            
+                    _piece = spawnedPiece.AddComponent<Piece>();
+            
+                    SpriteRenderer _renderer = spawnedPiece.AddComponent<SpriteRenderer>();
+                    _renderer.sprite = _piece.GetSprite(y == 0 ? "knight" : "knight1");
             }
             else if(x == 2 ||x == 5){
-                var spawnedPiece = Instantiate(_piecePrefab, new Vector3(x, y), Quaternion.identity);
-                    spawnedPiece.name = $"Bishop{y}";
-                    spawnedPiece.SetSprite(y == 0 ? "bishop" : "bishop1");
+                    GameObject spawnedPiece = new GameObject($"Bishop {x},{y}");
+                    spawnedPiece.transform.position = new Vector3(x, y, -3);
+            
+                    _piece = spawnedPiece.AddComponent<Piece>();
+            
+                    SpriteRenderer _renderer = spawnedPiece.AddComponent<SpriteRenderer>();
+                    _renderer.sprite = _piece.GetSprite(y == 0 ? "bishop" : "bishop1");
             }
             else if(x == 3){
-                var spawnedPiece = Instantiate(_piecePrefab, new Vector3(x, y), Quaternion.identity);
-                    spawnedPiece.name = $"{y}";
-                    spawnedPiece.SetSprite(y == 0 ? "king" : "queen1");
+                    GameObject spawnedPiece = new GameObject($"{x},{y}");
+                    spawnedPiece.transform.position = new Vector3(x, y, -3);
+            
+                    _piece = spawnedPiece.AddComponent<Piece>();
+            
+                    SpriteRenderer _renderer = spawnedPiece.AddComponent<SpriteRenderer>();
+                    _renderer.sprite = _piece.GetSprite(y == 0 ? "king" : "queen1");
             }
             else if(x == 4){
-                var spawnedPiece = Instantiate(_piecePrefab, new Vector3(x, y), Quaternion.identity);
-                    spawnedPiece.name = $"{y}";
-                    spawnedPiece.SetSprite(y == 0 ? "queen" : "king1");
+                    GameObject spawnedPiece = new GameObject($"{x},{y}");
+                    spawnedPiece.transform.position = new Vector3(x, y, -3);
+            
+                    _piece = spawnedPiece.AddComponent<Piece>();
+            
+                    SpriteRenderer _renderer = spawnedPiece.AddComponent<SpriteRenderer>();
+                    _renderer.sprite = _piece.GetSprite(y == 0 ? "queen" : "king1");
             }
             
         }
